b70870bf726577b5b847280ee14f9a63845d2fa5
John Melton - G0ORX/N6LYT <john.d.melton@googlemail.com>
2016-09-09 16:15:59 +0000

    switch from pulse audio to ALSA

--- release/pihpsdr/pulseaudio.sh
+++
@@ -1,7 +0,0 @@
-sudo usermod -a -G pulse-access pi
-sudo usermod -a -G audio root
-sudo usermod -a -G pulse root
-sudo usermod -a -G pulse-access root
-sudo cp pulseaudio.service /etc/systemd/system
-sudo systemctl --system enable pulseaudio.service
-sudo systemctl --system start pulseaudio.service
