1f827446f5b352aac61072f84f437fdfb0660241
John Melton - G0ORX <john.d.melton@googlemail.c>
2017-11-12 10:49:41 +0000

    updated release directory for 1.2

--- release/pihpsdr/install.sh
+++ release/pihpsdr/install.sh
@@ -1,23 +1,42 @@
+echo "installing fftw"
 sudo apt-get -y install libfftw3-3
-sudo apt-get -y install xscreensaver
-echo "removing old shared libraries"
+echo "removing old versions of pihpsdr"
+sudo rm -rf /usr/local/bin/pihpsdr
+echo "creating start script"
+cat <<EOT > start_pihpsdr.sh
+cd `pwd`
+sudo /usr/local/bin/pihpsdr
+EOT
+echo "creating desktop shortcut"
+cat <<EOT > pihpsdr.desktop
+#!/usr/bin/env xdg-open
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Name[eb_GB]=piHPSDR
+Exec=`pwd`/start_pihpsdr.sh
+Icon=`pwd`/hpsdr_icon.png
+Name=piHPSDR
+EOT
+cp pihpsdr.dshesktop ~/Desktop
+if [ ! -d "~/.local" ]; then
+  mkdir ~/.local
+fi
+if [ ! -d "~/.local/share" ]; then
+mkdir ~/.local/share
+fi
+if [ ! -d "~/.local/share/applications" ]; then
+mkdir ~/.local/share/applications
+fi
+cp pihpsdr.desktop ~/.local/share/applications
+echo "removing old versions of shared libraries"
 sudo rm -rf /usr/local/lib/libwdsp.so
-sudo rm -rf /usr/local/lib/libpsk.so
 sudo rm -rf /usr/local/lib/libcodec2.*
-sudo rm -rf /usr/local/lib/libSoapySDR.*
+echo "installing pihpsdr"
+sudo cp pihpsdr /usr/local/bin
 echo "installing shared libraries"
 sudo cp libwdsp.so /usr/local/lib
-sudo cp libpsk.so /usr/local/lib
-sudo cp libcodec2.so.0.5 /usr/local/lib
-sudo cp libSoapySDR.so.0.5-1 /usr/local/lib
-cd /usr/local/lib; sudo ln -s libcodec2.so.0.5 libcodec2.so
-cd /usr/local/lib; sudo ln -s libSoapySDR.so.0.5-1 libSoapySDR.so.0.5.0
-cd /usr/local/lib; sudo ln -s libSoapySDR.so.0.5-1 libSoapySDR.so
+sudo cp libcodec2.so.0.7 /usr/local/lib
+cd /usr/local/lib; sudo ln -s libcodec2.so.0.7 libcodec2.so
 sudo ldconfig
-echo "setting up for USB OZY device"
-cd ~/pihpsdr
-sudo cp 90-ozy.rules /etc/udev/rules.d
-sudo udevadm control --reload-rules
-echo "creating desktop icon"
-cp pihpsdr.desktop ~/Desktop
-cp pihpsdr.desktop ~/.local/share/applications
