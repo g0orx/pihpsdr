f403b976f0c9146529a90601e46996d756dcd4b1
c vw <dl1ycf@darc.de>
2020-11-05 10:12:40 +0100

    Replace MacOS MIDI device name by "NoPort" if it is empty.

--- mac_midi.c
+++ mac_midi.c
@@ -164,6 +164,12 @@ void register_midi_device(char *myname) {
 	    MIDIObjectGetStringProperty(dev, kMIDIPropertyName, &pname);
 	    CFStringGetCString(pname, name, sizeof(name), 0);
 	    CFRelease(pname);
+            //
+            // Some users have reported that MacOS reports a string of length zero
+            // for some MIDI devices. In this case, we replace the name by
+            // "NoPort"
+            //
+            if (strlen(name) == 0) strcpy(name,"NoPort");
 	    if (!strncmp(name, myname, mylen)) {
 		FoundMIDIref=i;
 		fprintf(stderr,"MIDI: registering device >%s<\n", name);
