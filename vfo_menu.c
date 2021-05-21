507672d315d3d412c7d6c45b683251cb6436e58c
c vw <dl1ycf@darc.de>
2021-05-21 20:00:42 +0200

    Corrected search in steps list

--- vfo_menu.c
+++ vfo_menu.c
@@ -1,4 +1,4 @@
-/* Copyright (C)
+ Copyright (C)
 * 2016 - John Melton, G0ORX/N6LYT
 *
 * This program is free software; you can redistribute it and/or
@@ -293,13 +293,11 @@ void vfo_menu(GtkWidget *parent,int vfo) {
   gtk_grid_attach(GTK_GRID(grid),vfo_label,3,3,1,1);
 
   GtkWidget *vfo_b=gtk_combo_box_text_new();
-  i=0;
-  while(steps[i]!=0) {
+  for (i=0; i<STEPS; i++) {
     gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(vfo_b),NULL,step_labels[i]);
     if(steps[i]==step) {
       gtk_combo_box_set_active (GTK_COMBO_BOX(vfo_b), i);
     }
-    i++;
   }
   g_signal_connect(vfo_b,"changed",G_CALLBACK(vfo_cb),NULL);
   gtk_grid_attach(GTK_GRID(grid),vfo_b,4,3,1,1);
